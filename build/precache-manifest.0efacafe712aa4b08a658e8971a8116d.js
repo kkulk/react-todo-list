204245096f3e52d5b8a18fea20d5db5da15bc64b
Kshitij Kulkarni <ksk@Kshitijs-MacBook-Pro.local>
2024-05-23 13:26:28 -0700

    todolist

---
+++ build/precache-manifest.0efacafe712aa4b08a658e8971a8116d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdc457d371e43b2e7c5693a558ed8b86",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b18a9162676684669798",
+    "url": "/static/js/2.b5c2370d.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.b5c2370d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c8d46097b8fc33fb8d34",
+    "url": "/static/js/main.58ba7704.chunk.js"
+  },
+  {
+    "revision": "e6f3d665f7ef55391fc6",
+    "url": "/static/js/runtime-main.d76c0f23.js"
+  }
+]);
